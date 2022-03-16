655bd137d8bd476e429607feb9bbcb3fe3a28f50
muaz <msultanic1@etf.unsa.ba>
2022-03-16 19:11:12 +0100

    Added server-side

---
+++ server/index.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const cors = require("cors");
+// const pool = require("./db");
+ const { sequelize, Todo } = require('../models')
+
+
+const app = express();
+app.use(express.json()); //req.body
+
+app.post("/todos", async (req, res) => {
+    const { description } = req.body;
+    try {
+      const todo = await Todo.create({ description })
+      return res.json(todo)
+    } catch (err) {
+      console.log(err)
+      return res.status(500).json(err)
+    }
+  })
+  
+  app.get('/todos', async (req, res) => {
+    try {
+      const todos = await Todo.findAll()
+      return res.json(todos)
+    } catch (err) {
+      console.log(err)
+      return res.status(500).json({ error: 'Something went wrong' })
+    }
+  })
+  
+  app.get('/todos/:id', async (req, res) => {
+    const id = req.params.id //trazi mi dvotacku??
+    try {
+      const todo = await Todo.findOne({
+        where: { id },
+        include: 'posts',
+      })
+  
+      return res.json(todo)
+    } catch (err) {
+      console.log(err)
+      return res.status(500).json({ error: 'Something went wrong' })
+    }
+  })
+  
+  app.delete('/todos/:id', async (req, res) => {
+    const id = req.params.id
+    try {
+      const todo = await Todo.findOne({ where: { id } })
+  
+      await todo.destroy()
+  
+      return res.json({ message: 'Todo deleted!' })
+    } catch (err) {
+      console.log(err)
+      return res.status(500).json({ error: 'Something went wrong' })
+    }
+  })
+  
+  app.put('/todos/:id', async (req, res) => {
+    const id = req.params.id
+    const { description } = req.body
+    try {
+      const todo = await Todo.findOne({ where: { id } })
+  
+      todo.description = description
+  
+      await todo.save()
+  
+      return res.json(todo)
+    } catch (err) {
+      console.log(err)
+      return res.status(500).json({ error: 'Something went wrong' })
+    }
+  })
+
+  app.listen({port:9000}, async() => {
+    console.log("server has started on port 9000")
+    await sequelize.authenticate()
+    // await sequelize.sync()
+  })
